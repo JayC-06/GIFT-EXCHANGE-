76e5d48efdafa81525a38ad2777d6fd5ff709d9e
Goose_tG <jarredhampton@hotmail.com>
2025-01-14 21:05:20 -0500

    browse items and start a trade redirect functionallity added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Gift exchange javascript
+// redirect function for browse items page
+function redirectToBrowseItems() {
+    window.location.href = 'item_copy.html';
+};
+
+document.getElementById('browseButton').addEventListener('click', redirectToBrowseItems);
+
+// redirect function for start a trade page
+function redirectToStartTrade() {
+    window.location.href = 'start_trade.html';
+};
+
+document.getElementById('startTradeButton').addEventListener('click', redirectToStartTrade);
